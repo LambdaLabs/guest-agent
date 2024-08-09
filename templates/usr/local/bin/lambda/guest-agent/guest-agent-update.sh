98abf4af6f1c5c62d051522ed6dd0698b2e3da8a
LandonTClipp <11232769+LandonTClipp@users.noreply.github.com>
2024-08-09 13:45:23 -0500

    update

--- templates/usr/local/bin/lambda/guest-agent/guest-agent-update.sh
+++ templates/usr/local/bin/lambda/guest-agent/guest-agent-update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 /usr/bin/apt update -o Dir::Etc::sourcelist="sources.list.d/lambda-guest-agent.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-/usr/bin/apt install {{ .GuestAgentServiceName }}
+/usr/bin/apt install lambda-guest-agent
