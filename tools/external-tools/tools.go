593137944005bbeef2065417f1e8b0ac938a4807
LandonTClipp <11232769+LandonTClipp@users.noreply.github.com>
2024-08-12 19:06:40 -0500

    updates

---
+++ tools/external-tools/tools.go
@@ -0,0 +1,6 @@
+package main
+
+import (
+	_ "github.com/go-task/task/v3"
+	_ "github.com/goreleaser/goreleaser/cmd"
+)
--- tools/tools/tools.go
+++
@@ -1,6 +0,0 @@
-package main
-
-import (
-	_ "github.com/go-task/task/v3/cmd/task"
-	_ "github.com/goreleaser/goreleaser"
-)
