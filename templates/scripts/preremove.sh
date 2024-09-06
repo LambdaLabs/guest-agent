299ef7534cc84fb7ada713e8f916d7058fffe1d7
LandonTClipp <11232769+LandonTClipp@users.noreply.github.com>
2024-09-06 13:47:49 -0500

    Fix issue with preremove step
    Removing the systemd services in preremove causes updates to fail because the updater
    service does not have time to complete the update before it gets whacked.

--- templates/scripts/preremove.sh
+++ templates/scripts/preremove.sh
@@ -1,8 +1,1 @@
 #!/bin/bash
-
-systemctl disable {{ .GuestAgentServiceName }}
-systemctl disable {{ .GuestAgentUpdaterServiceName }}
-systemctl disable {{ .GuestAgentUpdaterTimerName }}
-systemctl stop {{ .GuestAgentServiceName }}
-systemctl stop {{ .GuestAgentUpdaterServiceName }}
-systemctl stop {{ .GuestAgentUpdaterTimerName }}
