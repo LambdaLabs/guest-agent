0dbbc2ad05d85478e8167208920e95064e485128
LandonTClipp <11232769+LandonTClipp@users.noreply.github.com>
2024-09-12 18:11:37 -0500

    Fix issue when updater service triggers an upgrade
    The package installation would fail because `postremove.sh` would prematurely
    kill the updater service before `apt install` had a chance to complete. This
    completely broke updates.

--- templates/scripts/postremove.sh
+++ templates/scripts/postremove.sh
@@ -4,5 +4,10 @@ systemctl disable {{ .GuestAgentServiceName }}
 systemctl disable {{ .GuestAgentUpdaterServiceName }}
 systemctl disable {{ .GuestAgentUpdaterTimerName }}
 systemctl stop {{ .GuestAgentServiceName }}
-systemctl stop {{ .GuestAgentUpdaterServiceName }}
 systemctl stop {{ .GuestAgentUpdaterTimerName }}
+# Note: we can't stop the updater service itself because it needs
+# to run to completion during the course of the package installation.
+# If we stop it in this script, the apt install command triggered by
+# lambda-guest-agent-updater.service will fail because this script would
+# be prematurely halting it before completion. It's equivalent to SIGTERM-ing
+# "apt install" halfway through its execution.
--- templates/usr/local/bin/lambda/guest-agent/guest-agent-update.sh
+++ templates/usr/local/bin/lambda/guest-agent/guest-agent-update.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 apt update -o Dir::Etc::sourcelist="sources.list.d/lambda-guest-agent.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
-old_version="$(apt info lambda-guest-agent |& grep 'Version' | awk '{print $2}')"
 apt install lambda-guest-agent
-new_version="$(apt info lambda-guest-agent |& grep 'Version' | awk '{print $2}')"
-if [ "$old_version" -ne "$new_version"]; then   
-    echo "upgraded $old_version to $new_version, restarting service".
-    systemctl restart {{ .GuestAgentServiceName }}
-else
-    echo "no new version installed"
-fi
